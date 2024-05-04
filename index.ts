612883879b18f9eaf557405bada0294d3118df29
hookTools <kucherenko.alexandr1998@gmail.com>
2024-05-05 01:40:20 +0300

    add

--- index.ts
+++ index.ts
@@ -16,15 +16,24 @@ const mintBoB = async (privateKey:`0x${string}`) => {
         transport: http(),
       })
 
-      const hash = await client.writeContract({
-        address:"0x8a6f282b15c595bf07aaCaC6Cba245cE427CCBb4",
-        abi: mintABI,
-        functionName: 'mint',
-        gasPrice: parseGwei('0.0015'),
-        gas: BigInt("170000")
-      })
-      log(chalk.blue(`${account.address}:`),hash);
+      let i = 0
+      while(i < 3) {
+        try{
+            const hash = await client.writeContract({
+                address:"0x8a6f282b15c595bf07aaCaC6Cba245cE427CCBb4",
+                abi: mintABI,
+                functionName: 'mint',
+                gasPrice: parseGwei('0.0016'),
+                gas: BigInt("160000")
+              })
+              log(chalk.blue(`${account.address}:`),hash);
+              break
+        } catch(e) {
+            i++
+        }
+      }
     }
+
 const getBlock = async () => {
     const keys = (await fs.promises.readFile('keys.txt', 'utf-8')).split('\n') as `0x${string}`[]
     const BobClint = createPublicClient({
@@ -43,7 +52,7 @@ const getBlock = async () => {
         log(chalk.blue(`Current state:`),mintState);
         if(mintState === 4) {
             for (const key of keys) {
-                mintBoB(key)
+                mintBoB(key).catch(e => console.log(e.message))
                 await new Promise((res) => {
                     setTimeout(() => res(null), 50)
                 })
